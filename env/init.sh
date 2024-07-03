3f0d4cbe3d8468a8674750edab416e21c98c5716
UoFixED <42113547+UoFixED@users.noreply.github.com>
2024-07-03 02:25:04 +0200

    test2

--- env/init.sh
+++ env/init.sh
@@ -6,6 +6,10 @@ sudo apt install net-tools -y
 
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
+curl -s https://raw.githubusercontent.com/karmada-io/karmada/master/hack/install-cli.sh | sudo INSTALL_CLI_VERSION=1.10.2 bash
+
+helm repo add cilium https://helm.cilium.io/
+
 # curl -LO https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl
 # mv /root/package/kubectl /usr/local/bin/kubectl
 # chmod 777 /usr/local/bin/kubectl
@@ -17,6 +21,16 @@ do
 echo "  apiServerAddress: \"$(ifconfig eno1 |grep "inet " | cut -f 10 -d " ")"\" >> /root/ddos/env/config/kind-example-config-$i.yaml
 done
 
-kind create cluster --config /root/ddos/env/config/kind-example-config-1.yaml --name management0 &
-kind create cluster --config /root/ddos/env/config/kind-example-config-2.yaml --name member0 &
-kind create cluster --config /root/ddos/env/config/kind-example-config-3.yaml --name member1 &
+kind create cluster --config /root/ddos/env/config/kind-example-config-1.yaml --name cluster0 &
+kind create cluster --config /root/ddos/env/config/kind-example-config-2.yaml --name cluster1 &
+kind create cluster --config /root/ddos/env/config/kind-example-config-3.yaml --name cluster2
+
+sleep 20
+
+for j in `seq 1 3`
+		do
+			kubectl config use-context kind-cluster$j
+			helm repo update
+			helm install cilium cilium/cilium --version 1.15.6 --namespace kube-system --set cluster.name=cluster$j --set cluster.id=$j
+		done
+echo "----------------$i OK----------------"
